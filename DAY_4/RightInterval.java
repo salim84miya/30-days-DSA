0a06e23da6c00f1bd12cd9dd43453214e7c58363
salim84miya <salimsaudagar84@gmail.com>
2024-11-22 23:11:32 +0530

    fifth day of right interval solved

--- DAY_4/RightInterval.java
+++ DAY_4/RightInterval.java
@@ -14,47 +14,64 @@ public class RightInterval {
 
     }
 
-    static int[] rightInterval(int[][] arr){
 
+    //for iterating over the array
+    private static int[] rightInterval(int[][] arr){
+
+        //check for single or element present case
         if(arr.length==0 || arr.length==1){
             return new int[]{-1};
         }
 
+        //storing answer
         int[] ans = new int[arr.length];
 
         for(int i =0; i<arr.length; i++){
-            int start =0;
-            int end = arr.length-1;
 
-            boolean isAscending = arr[start][0]<arr[end][0];
-
-            while(start<=end){
-                int mid = start+(end-start)/2;
-                if(arr[i][1]==arr[mid][0]){
-                    ans[i] = mid;
-                    break;
-                }else if(isAscending){
-                    if(arr[i][1]>arr[mid][0]){
-                        start = mid+1;
-                    }else{
-                        end = mid-1;
-                    }
-                }else{
-                    if(arr[i][1]>arr[mid][0]){
-                        end = mid-1;
-                    }else{
-                        start = mid+1;
-                    }
-                }
-            }
-
-            if(arr[start][0]>=arr[i][1]){
-                ans[i] = start;
-            }else{
-                ans[i]= -1;
-            }
+            //bs for every end of the internal arrays
+          ans[i] = binarySearch2d(arr, arr[i][1], i);
         }
 
         return ans;
+    }
+
+
+    //actual bs method for checking
+    //doing order augostic bs
+    private static int binarySearch2d(int[][] arr,int target,int i){
+       
+        int start =0;
+        int end = arr.length-1;
+
+        boolean isAscending = arr[start][0]<arr[end][0];
+
+        while(start<=end){
+            int mid = start+(end-start)/2;
+           
+            if(target==arr[mid][0]){
+                //return mid if start value are same to target elements end
+                return mid;
+            }else if(isAscending){
+                if(arr[i][1]>arr[mid][0]){
+                    start = mid+1;
+                }else{
+                    end = mid-1;
+                }
+            }else{
+                if(arr[i][1]>arr[mid][0]){
+                    end = mid-1;
+                }else{
+                    start = mid+1;
+                }
+            }
+        }
+   
+        //if start value is greater than target value means it is the greatest minimized value so return it
+        if(arr[start][0]>=arr[i][1]){
+            return start;
+        }else{
+            //else no value present add -1
+            return -1;
+        }
     }
 }
